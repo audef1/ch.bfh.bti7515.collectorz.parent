e5f397e4778613ed5a53c1d92f8fa12aee6dcfd4
Florian Auderset <f_auderset@hotmail.com>
2016-10-27 17:43:34 +0200

    add persistance unit and entities

--- collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/model/Collection.java
+++ collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/model/Collection.java
@@ -1,8 +1,10 @@
 package ch.bfh.bti7515.collectorz.gamecollector.model;
 
+import javax.persistence.Entity;
+
 /**
  * Created by florianauderset on 27.10.16.
  */
+@Entity
 public class Collection {
-
 }
--- collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/model/Platform.java
+++ collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/model/Platform.java
@@ -1,9 +1,10 @@
 package ch.bfh.bti7515.collectorz.gamecollector.model;
 
-/**
- * Created by florianauderset on 27.10.16
- * formerly called "Console" in first draft of the domain model
- */
-public class Platform {
+import javax.persistence.Entity;
 
+/**
+ * Created by florianauderset on 27.10.16.
+ */
+@Entity
+public class Platform {
 }
