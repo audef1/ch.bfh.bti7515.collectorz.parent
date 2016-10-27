94b38ecef75dc11658944bd01b61016eb2ef1b54
Florian Auderset <f_auderset@hotmail.com>
2016-10-27 17:15:11 +0200

    add empty models

---
+++ collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/model/Platform.java
@@ -0,0 +1,9 @@
+package ch.bfh.bti7515.collectorz.gamecollector.model;
+
+/**
+ * Created by florianauderset on 27.10.16
+ * formerly called "Console" in first draft of the domain model
+ */
+public class Platform {
+
+}
---
+++ collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/model/Publisher.java
@@ -0,0 +1,8 @@
+package ch.bfh.bti7515.collectorz.gamecollector.model;
+
+/**
+ * Created by florianauderset on 27.10.16.
+ */
+public class Publisher extends Company {
+
+}
