f0beac37572fa0da1c2dd12c77f719d79944255a
Florian Auderset <f_auderset@hotmail.com>
2016-11-10 17:05:55 +0100

    removed .idea-folder

---
+++ collectorz.domain/src/main/java/Test.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by florianauderset on 10.11.16.
+ */
+public class Test {
+}
---
+++ collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/repository/GameRepository.java
@@ -0,0 +1,7 @@
+package ch.bfh.bti7515.collectorz.gamecollector.repository;
+
+/**
+ * Created by florianauderset on 10.11.16.
+ */
+public class GameRepository {
+}
