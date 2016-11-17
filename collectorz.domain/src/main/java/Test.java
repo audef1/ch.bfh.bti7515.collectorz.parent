8b722c2d69921ca250bd8621b150492d437956f0
Florian Auderset <f_auderset@hotmail.com>
2016-11-17 16:39:29 +0100

    added some repositories

--- collectorz.domain/src/main/java/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by florianauderset on 10.11.16.
- */
-public class Test {
-}
--- collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/repository/CollectorRepository.java
+++ collectorz.domain/src/main/java/ch/bfh/bti7515/collectorz/gamecollector/repository/CollectorRepository.java
@@ -1,5 +1,8 @@
 package ch.bfh.bti7515.collectorz.gamecollector.repository;
 
+import ch.bfh.bti7515.collectorz.gamecollector.model.Collector;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by florianauderset on 10.11.16.
  */
