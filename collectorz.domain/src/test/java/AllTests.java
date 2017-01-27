3816f8dd15815a37dca48db567d7d0eaf73eb95e
Michael Räss <michael.raess@payrexx.com>
2017-01-27 16:48:16 +0100

    Initialized projects, fixed warnings

--- collectorz.domain/src/test/java/AllTests.java
+++ collectorz.domain/src/test/java/AllTests.java
@@ -2,8 +2,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import ch.bfh.bti7515.collectorz.gamecollector.model.*;
-
 @RunWith(Suite.class)
 @SuiteClasses({ CreateIT.class, ReadIT.class, UpdateIT.class, DeleteIT.class})
 public class AllTests {
